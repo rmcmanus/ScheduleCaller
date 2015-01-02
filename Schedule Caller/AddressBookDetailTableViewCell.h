25794229d1189eb86367eed4c8a8b9c6e52db287
Ryan McManus <ryan.mcmanus10@gmail.com>
2015-01-02 00:26:48 -0700

    Added table view cell

---
+++ Schedule Caller/AddressBookDetailTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  AddressBookDetailTableViewCell.h
+//  Schedule Caller
+//
+//  Created by Ryan McManus on 1/2/15.
+//  Copyright (c) 2015 Ryan McManus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddressBookDetailTableViewCell : UITableViewCell
+
+@end
---
+++ Schedule Caller/AddressBookDetailTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  AddressBookDetailTableViewCell.m
+//  Schedule Caller
+//
+//  Created by Ryan McManus on 1/2/15.
+//  Copyright (c) 2015 Ryan McManus. All rights reserved.
+//
+
+#import "AddressBookDetailTableViewCell.h"
+
+@implementation AddressBookDetailTableViewCell
+
+@end
